ff21a7f8f4d886d11d4d314124f0651a70599c92
sviatoslav <www.t.svyatoslav55@gmail.com>
2023-10-19 17:30:10 +0200

    innitial commit

---
+++ android/app/src/main/kotlin/com/example/my_learm_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_learm_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Text('Plugin example app'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
